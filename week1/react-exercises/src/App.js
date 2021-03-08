f69aef11679df20b077e1c55d6833ae647038742
mas <mas.sam1368@gmail.com>
2021-03-08 21:06:06 +0330

    Ex1

---
+++ week1/react-exercises/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { HobbyList } from './extremeHobbies'
+
+function App() {
+  return (
+    <div className="App">
+      <HobbyList />
+    </div>
+  );
+}
+
+export default App;
