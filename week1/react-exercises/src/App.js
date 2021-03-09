117aafa72c90d62dbc4def175a59f49611f6bfec
Amirhossein <ahshadravan@gmail.com>
2021-03-09 08:05:45 +0330

    Exer1 is added

---
+++ week1/react-exercises/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import HobbyList from './HobbyList';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <HobbyList />
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
