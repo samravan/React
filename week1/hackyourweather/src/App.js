7b6d106a17693cad35a9c5b978560fd2ffa87bcc
Amirhossein <ahshadravan@gmail.com>
2021-03-14 09:03:17 +0330

    Minor changes

--- week1/hackyourweather/src/App.js
+++ week1/hackyourweather/src/App.js
@@ -4,8 +4,8 @@ import Weather from './Weather'
 
 const data = require('./city-weather.json');
 const citiesList = data.map((items, index) => <Weather key={index} name={items.name} country={items.sys.country} weather={items.main.temp} description={items.weather[0].description} temp_max={items.main.temp_max} temp_min={items.main.temp_min} lon={items.coord.lon} lat={items.coord.lat} />)
-function App() {
 
+function App() {
   return (
     <div className="App">
       {citiesList}
