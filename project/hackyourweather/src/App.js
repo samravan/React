8d5b29f4323c0789302cf0702161bf0547ba35c6
Amirhossein <ahshadravan@gmail.com>
2021-04-10 08:12:16 +0430

    Project is intialized

--- project/hackyourweather/src/App.js
+++ project/hackyourweather/src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import Weather from './Weather'
 
-const data = require('./city-weather.json');
-const citiesList = data.map((items, index) => <Weather key={index} name={items.name} country={items.sys.country} weather={items.main.temp} description={items.weather[0].description} temp_max={items.main.temp_max} temp_min={items.main.temp_min} lon={items.coord.lon} lat={items.coord.lat} />)
+// const data = require('./city-weather.json');
+// const citiesList = data.map((items, index) => <Weather key={index} name={items.name} country={items.sys.country} weather={items.main.temp} description={items.weather[0].description} temp_max={items.main.temp_max} temp_min={items.main.temp_min} lon={items.coord.lon} lat={items.coord.lat} />)
 
 function App() {
   return (
-    <div>
-      {citiesList}
+    <div className='container'>
+      <h1>Weather App</h1>
+      <Weather />
+
+        {/* {citiesList} */}
+
     </div>
 
   );
--- project/hackyourweather/src/Weather.js
+++ project/hackyourweather/src/Weather.js
@@ -1,15 +1,50 @@
-const Weather = (props) => {
-    return (
-    <div className="box">
-        <h2>{props.name}</h2>
-        <h2>{props.country}</h2>
-        <h3>{props.weather}</h3>
-        <h4>{props.description}</h4>
-        <p>min temp: {props.temp_min}</p>
-        <p>max temp: {props.temp_max}</p>
-        <p>location: {props.lon}, {props.lat}</p>
+import React, {useState} from 'react';
 
-    </div>
+
+const Weather = () => {
+    const [city, setCity] = useState();
+    const [isLoading, setIsLoading] = useState(true);
+    const [hasError, setHasError] = useState(false);
+    const [getData, setGetData] = useState();
+
+    const getWeather = () => {
+        setIsLoading(true)
+        const API_KEY = process.env.REACT_APP_OPENWEATHERMAP_API_KEY;
+        return fetch(`api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`)
+                .then(res => res.json())
+                .then(data => setGetData(data))
+                .catch(error => console.log(error))
+
+    }
+
+    return <WeatherProfile />
+}
+
+const WeatherProfile = (props) => {
+    return (
+        <div className="box">
+            <Form />
+            {/* <h2>{props.name}</h2>
+            <h2>{props.country}</h2>
+            <h3>{props.weather}</h3>
+            <h4>{props.description}</h4>
+            <p>min temp: {props.temp_min}</p>
+            <p>max temp: {props.temp_max}</p>
+            <p>location: {props.lon}, {props.lat}</p> */}
+        </div>
+    )
+}
+
+const Form = (props) => {
+    return (
+        <form className="searchBox" onsubmit={props.getWeather}>
+            <input className="searchInput"type="text" name="" placeholder="Search"/>
+            <button className="searchButton" type="submit" href="#" value={props.city}>
+                <i className="material-icons">
+                    search
+                </i>
+            </button>
+        </form>
     )
 }
 
