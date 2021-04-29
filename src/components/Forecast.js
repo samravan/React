b3f6fbf3a0ca9b57ffdc9a61afcc059614793244
Masoud <mas.sam1368@gmail.com>
2021-04-29 14:02:59 +0430

    get it responsive

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -10,7 +10,8 @@ import {
   YAxis,
   Tooltip,
   AreaChart,
-  Area
+  Area,
+  ResponsiveContainer
 } from "recharts";
 
 const Forecast = () => {
@@ -23,24 +24,26 @@ const Forecast = () => {
         <Link to='/'><button className='backButton'>Back</button></Link>
       </div>
       {loading ? <h3>Loading</h3> :
-       <AreaChart className='chart' style={{ textAlign: 'center', backgroundColor: '#E7E3D4' }} width={730} height={250} data={forecastData}
-       margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
-       <defs>
-         <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-           <stop offset="5%" stopColor="#B23850" stopOpacity={0.8} />
-           <stop offset="100" stopColor="#B23850" stopOpacity={0} />
-         </linearGradient>
-         <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-           <stop offset="5%" stopColor="#B23850" stopOpacity={0.8} />
-           <stop offset="95%" stopColor="#B23850" stopOpacity={0} />
-         </linearGradient>
-       </defs>
-       <XAxis dataKey="date" />
-       <YAxis />
-       <CartesianGrid strokeDasharray="3 3" />
-       <Tooltip />
-       <Area type="monotone" dataKey="temp" stroke="#B23850" fillOpacity={1} fill="url(#colorUv)" />
-     </AreaChart> 
+      <ResponsiveContainer width='100%' height={400}>
+        <AreaChart className='chart' data={forecastData}
+        margin={{ top: 10, right: 0, left: 0, bottom: 0 }}>
+        <defs>
+          <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="5%" stopColor="#B23850" stopOpacity={0.8} />
+            <stop offset="100" stopColor="#B23850" stopOpacity={0} />
+          </linearGradient>
+          <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="5%" stopColor="#B23850" stopOpacity={0.8} />
+            <stop offset="95%" stopColor="#B23850" stopOpacity={0} />
+          </linearGradient>
+        </defs>
+        <XAxis dataKey="date" />
+        <YAxis />
+        <CartesianGrid strokeDasharray="3 3" />
+        <Tooltip />
+        <Area type="monotone" dataKey="temp" stroke="#B23850" fillOpacity={1} fill="url(#colorUv)" />
+      </AreaChart>
+     </ResponsiveContainer> 
      }
      
     </>
